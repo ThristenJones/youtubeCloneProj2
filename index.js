ff0c94c87521cc1ff27e0aca34c3d58a1203677a
ThristenJones <Thristenjones@yahoo.com>
2021-10-12 21:13:09 -0500

    installed express, node modules, and config to projext

---
+++ index.js
@@ -0,0 +1,14 @@
+const connectDB = require('./startup/db');
+const express = require('express');
+const app = express();
+
+
+connectDB();
+
+app.use(express());
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`server started on port: ${port}`);
+});
+const
---
+++ models/videos.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const replySchema = new mongoose.Schema({
+    text: {type: String, required: true, minlength: 5, maxlength: 1000},
+    timeStamp: {type: Date, default: Date.now()},
+});
+
+const commentSchema = new mongoose.Schema({
+    text: {type: String, required: true, minlength: 5, maxlength: 1000},
+    videoId: {type: String, required: true},
+    timeStamp: {type: Date, default: Date.now()},
+    likes: {type: Number, default: 0},
+    dislikes: {type: Number, default: 0},
+    replies: [{type: replySchema}],
+})
+
+const Reply = mongoose.model('Reply', replySchema);
+const Comment = mongoose.model('Comment', commentSchema);
+
+//validation
+
+//text: Joi.string().required().min(5).max(1000)
+
+module.exports = {
+    Reply,
+    Comment,
+    //validateComment
+    //validateReply
+}
