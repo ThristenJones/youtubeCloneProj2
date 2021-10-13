0613701e057ed66d6ded84d5601f6a5f17c37b33
ThristenJones <Thristenjones@yahoo.com>
2021-10-13 18:44:39 -0500

    added express nodemon and node modules

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const connectDB = require('./startup/db');
 const express = require('express');
 const app = express();
+const videos = require('./routes/videos');
+const { Router } = require('express');
 
 
 connectDB();
 
-app.use(express());
+app.use(express.json());
+app.use('/api/videos', videos);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server started on port: ${port}`);
-})
-const
+});
+
+
